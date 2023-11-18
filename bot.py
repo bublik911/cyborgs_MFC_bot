93d3e306b834e6a69ff3899e6fc631b6f8f8f077
Egor <ekurlenko17@gmail.com>
2023-11-18 10:48:48 +0300

    create_event_handler

--- bot.py
+++ bot.py
@@ -1,12 +1,13 @@
 import asyncio
-from handlers import start
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from handlers import start, create_event
 from aiogram import Bot, Dispatcher
 
 
 async def main():
     bot = Bot(token="6635292265:AAH8uLVCLZ1RD44J_an5AZ6tBIrisJXwjqI")
     dp = Dispatcher()
-    dp.include_routers(start.router)
+    dp.include_routers(start.router, create_event.router)
     await dp.start_polling(bot)
 
 
