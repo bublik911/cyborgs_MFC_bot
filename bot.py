fd9b317a26605681352f383b0c4874b9678f1339
Egor <ekurlenko17@gmail.com>
2023-11-20 14:58:34 +0300

    scheduler task

--- bot.py
+++ bot.py
@@ -1,13 +1,17 @@
 import asyncio
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from handlers import start, create_event
+from handlers import start, create_event, marking
 from aiogram import Bot, Dispatcher
+from misc.utils import notification_for_chatbot
 
 
 async def main():
     bot = Bot(token="6635292265:AAH8uLVCLZ1RD44J_an5AZ6tBIrisJXwjqI")
     dp = Dispatcher()
-    dp.include_routers(start.router, create_event.router)
+    scheduler = AsyncIOScheduler()
+    dp.include_routers(start.router, create_event.router, marking.router)
+    scheduler.add_job(notification_for_chatbot, "interval", seconds=30, args=(bot,))
+    scheduler.start()
     await dp.start_polling(bot)
 
 
