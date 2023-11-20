f3c77fd17586786eb89427f2bb436f2bb53db3a2
Egor <ekurlenko17@gmail.com>
2023-11-20 14:57:42 +0300

    callbacks added

--- keyboards/answer.py
+++ keyboards/answer.py
@@ -1,8 +1,9 @@
 from aiogram.utils.keyboard import InlineKeyboardBuilder, InlineKeyboardMarkup
+from callbacks.mark_callback import MarkCallbackFactory
 
 
-def answer_yes_no() -> InlineKeyboardMarkup:
+def answer_yes_no(id: int) -> InlineKeyboardMarkup:
     keyboard = InlineKeyboardBuilder()
-    keyboard.button(text="Я пойду", callback_data="yes")
-    keyboard.button(text="Я не пойду", callback_data="no")
+    keyboard.button(text="Я пойду", callback_data=MarkCallbackFactory(event_id=id, status=1))
+    keyboard.button(text="Я не пойду", callback_data=MarkCallbackFactory(event_id=id, status=0))
     return keyboard.as_markup()
