267aa6fb58f997aee06cc1b8eca37c83ec3059ad
Egor <ekurlenko17@gmail.com>
2023-11-26 00:02:17 +0300

    create massage was replaced

--- handlers/marking.py
+++ handlers/marking.py
@@ -3,7 +3,7 @@ from aiogram.types import CallbackQuery
 
 from misc import env
 from misc.constValues import FIRST_DIVISION, SECOND_DIVISION
-from misc.utils import create_names_list
+from misc.utils import create_names_list, create_message_for_chat
 
 from callbacks.mark_callback import MarkCallbackFactory
 
@@ -27,17 +27,14 @@ async def marking(callback: CallbackQuery, callback_data: MarkCallbackFactory):
         MarkRepository.update_mark_status_yes(player_id, event_id)
         await bot.send_message(chat_id=chat_id, text="Отлично, ждем тебя!")
         event = EventRepository.get_event_by_event_id(event_id)
-        names = create_names_list(event_id)
-        message = ("Всем привет!\n"
-                   f"{event.type_of_event}, {event.place}\n"
-                   f"{event.date} в {str(event.time)[:5]}\n"
-                   f"Сегодня будут:\n")
-        list = "\n".join(names)
+
+        message = create_message_for_chat(event)
+        player_names_list = "\n".join(create_names_list(event_id))
 
         if event.place == FIRST_DIVISION:
-            await bot.send_message(chat_id=-1001999805953, text=message + list, message_thread_id=5)
+            await bot.send_message(chat_id=-1001999805953, text=message + player_names_list, message_thread_id=5)
         elif event.place == SECOND_DIVISION:
-            await bot.send_message(chat_id=-1001999805953, text=message + list, message_thread_id=4)
+            await bot.send_message(chat_id=-1001999805953, text=message + player_names_list, message_thread_id=4)
 
     elif status == 0:
         await bot.send_message(chat_id=chat_id, text="Жаль, ждем тебя в следующий раз!")
