84d0ca2c520e1cb107d26d23ca9c62245942a058
Egor <ekurlenko17@gmail.com>
2023-11-26 00:05:33 +0300

    str values was replaced for const Values

--- keyboards/event_type.py
+++ keyboards/event_type.py
@@ -1,11 +1,12 @@
 from aiogram.utils.keyboard import ReplyKeyboardBuilder, ReplyKeyboardMarkup
 
+from misc.constValues import TRAINING, GAME
+
 
 def event_type_keyboard() -> ReplyKeyboardMarkup:
     keyboard = ReplyKeyboardBuilder()
-    keyboard.button(text="Тренировка")
-    keyboard.button(text="Матч")
+    keyboard.button(text=TRAINING)
+    keyboard.button(text=GAME)
     return keyboard.as_markup(
         one_time_keyboard=True,
-        resize_keyboard=True
-    )
+        resize_keyboard=True)
