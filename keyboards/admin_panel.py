fa90e5d0d3820d0710fec745d8a4039289bb876e
Egor <ekurlenko17@gmail.com>
2024-01-03 00:03:15 +0300

    consts

--- keyboards/admin_panel.py
+++ keyboards/admin_panel.py
@@ -1,10 +1,13 @@
 from aiogram.utils.keyboard import ReplyKeyboardBuilder, ReplyKeyboardMarkup
 
+from misc.constValues import CHECK_PLAYERS_LIST, ADD_PLAYER, ADD_EVENT
+
 
 def admin_panel_keyboard() -> ReplyKeyboardMarkup:
     keyboard = ReplyKeyboardBuilder()
-    keyboard.button(text="Создать событие")
-    keyboard.button(text="Добавить игрока")
+    keyboard.button(text=ADD_EVENT)
+    keyboard.button(text=ADD_PLAYER)
+    keyboard.button(text=CHECK_PLAYERS_LIST)
     return keyboard.as_markup(
         one_time_keyboard=True,
         resize_keyboard=True
