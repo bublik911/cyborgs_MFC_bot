d0256ecd7a537bf7e98ec64507da1be911c70b49
Egor <ekurlenko17@gmail.com>
2024-01-04 00:31:20 +0300

    refactor

--- DataBase/migrations/PlayerMigration.py
+++ DataBase/migrations/PlayerMigration.py
@@ -1,6 +1,13 @@
 from playhouse.migrate import *
 
-from DataBase.config import db
+from peewee import MySQLDatabase
+
+db = MySQLDatabase(
+    host='127.0.0.1',
+    user='user', #//user
+    password='Root767!',
+    database='cyborgs',
+    charset='utf8mb4')
 
 deleted_at = DateField(null=True)
 
