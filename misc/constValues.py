e9a7bbf6c8220555d696ad7af8c108e93e2eed62
Egor <ekurlenko17@gmail.com>
2024-01-03 21:23:10 +0300

    Кнопки проверки списка игроков на корректность

--- misc/constValues.py
+++ misc/constValues.py
@@ -3,6 +3,10 @@ ADD_PLAYER = "Добавить игрока"
 ADD_EVENT = "Создать событие"
 CHECK_PLAYERS_LIST = "Список игроков"
 
+#Кнопки проверки списка игроков на корректность
+TABLE_IS_CORRECT = "Все верно"
+DELETE_PLAYER = "Удалить игрока"
+
 #Тип дивизиона
 FIRST_DIVISION = "ИАТЭ"
 SECOND_DIVISION = "Манеж"
